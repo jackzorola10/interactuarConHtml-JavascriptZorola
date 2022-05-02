6ed7dcb9ae1d8880bffa416e84b53c9d6f16b038
Jack Zorolasl <jackzorola10@gmail.com>
2022-05-02 11:51:49 -0500

    added documentation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Tome el javascript de la entrega anterior y lo usaré para representar estos datos en el DOM. 
+// Tome el javascript de la entrega anterior y lo usé para representar los datos en el DOM. 
 // Concepto: Le presentamos al usuario una lista de canciones, compuesta por un array de objetos, las cuales el usuario a traves de ingresar algunos datos alterará y se le serán presentados de vuelta. 
 
 /// Array de 20 canciones, con los siguientes campos: id, name, autor, genero y tiempo
@@ -26,8 +26,10 @@
         {   id:20 , name: "The Bidding", autor: "Tally Hall", genero:"Rock", tiempo: 192   },
     ]
         
-    // Toda esta sección, toma un array y sus distintos valores (los cuales no he logrado generalizar) y los imprime en un string que puedes llamar para colocarlo facilmente en un console.log. Esto es solo para presentarlo de una forma mas estetica al usuario.
-    let printedMenu;
+    // Toda esta sección, toma un array y sus distintos valores (los cuales no he logrado generalizar) y los imprime en un string que puedes llamar para colocarlo facilmente en un console.log o en un texto. 
+    // Esto es solo para presentarlo de una forma mas estetica al usuario.
+
+    let printedMenu; // Esta variable guarda el valor final que se usará
     const songMenu = [];
     function printingMenu (valor) {
         songMenu.splice(0, songMenu.length);
@@ -49,47 +51,49 @@
     // #######################################################################
     // FLUJO DEL USUARIO #####################################################
     // #######################################################################
-    
+    let userGenre; // Selección de genero del usuario por el cual filtraremos la lista.
+    let songFilter;  // Lista filtrada
+    let userTimeAdd // Cantidad de tiempo que el usuario quiere agregar a las canciones.
+
     // Se le presenta la playlist general al usuario.
     
     let message = document.getElementById("playlist");
     message.innerText = "Por favor ayudanos a crear una playlist ideal para nuestros castorescuchas. \n" + "\nEsta es nuestra playlist actual: \n" + printedMenu;
 
     alert("Bienvenido a radio CH-Jack, la unica radio enfocada 100% en musica para castores.");
-    let userGenre;
-    let songFilter; 
 
-    const getValueInput = () => {
-        let inputValue = document.getElementById("genre").value; 
-        userGenre = inputValue;
-        console.log(userGenre);
-        let eraseArea = document.getElementById("actionArea");
+    // Le presentamos un input al usuario el cual este podra usar para ingresar el genero por el cual queremos que filtre. 
+    const getValueInput = () => { // Activado a partir del click del usuario en el boton.
+        let inputValue = document.getElementById("genre").value;  // Se obtiene el valor del input
+        userGenre = inputValue; // Se extrae a la variable global
+        console.log(userGenre); // Imprimimos para validación
+        let eraseArea = document.getElementById("actionArea"); // Borramos el area para que el usuario deje de interactuar con ella.
         eraseArea.remove();
-        songFilter = songs.filter(   (el) => el.genero.includes(userGenre)  );
+        songFilter = songs.filter(   (el) => el.genero.includes(userGenre)  ); // Utilizamos la información para filtrar el array. IMPORTANTE por ahora reomoví la validación de datos porque no estoy seguro como armar un flujo considerando el DOM aun. 
         console.log(songFilter);
-        newFilteredPlaylist();
+        newFilteredPlaylist(); // Activa la siguiente sección del flujo. 👇🏼
       }
 
-    let userTimeAdd
-
+    // Creamos una nueca sección en la que colocamos un nuevo input y un nuevo boton.
     const newFilteredPlaylist = () => {
-        let newPlaylist = document.createElement("section");
-        newPlaylist.className = "actionArea";
+        let newPlaylist = document.createElement("section"); // Creamos una <sección>. 
+        newPlaylist.className = "actionArea"; // Le agregamos la clase "actionArea"
         newPlaylist.innerHTML = "<p>Los castores detestan cuando una canción termina y empieza la siguiente inmediatamente. <br><br>¿Cuantos segundos deberiamos esperar entre canción y canción?</p>" + 
         "<input type='text' id='timeAdditionInput'>" +
-        "<button id='timeAdditionButton' type='button'  onclick='getTimeInput()'> click me!! </button> ";
-        document.body.append(newPlaylist);
+        "<button id='timeAdditionButton' type='button'  onclick='getTimeInput()'> click me!! </button> "; // Imprimimos el resto de los elementos dentro de ella.
+        document.body.append(newPlaylist); // Se coloca en el HTML
         console.log("milestone 1");
     }
 
+    // Obtenemos el dato numerico para la segunda pregunta del usuario. ¿Cuantos segundos deberiamos agregar?
     let getTimeInput = () => {
         console.log("milestone 2")
-        let timeAddedByUser = document.getElementById("timeAdditionInput").value;
+        let timeAddedByUser = document.getElementById("timeAdditionInput").value; 
         userTimeAdd = timeAddedByUser;
         console.log("userTimeAdd is " + userTimeAdd)
-        let eraseArea = document.getElementById("actionArea");
+        let eraseArea = document.getElementById("actionArea"); // Volvemos a borrar el area para evitar que usuario interactue con ella.
         console.log("milestone 3");
-        songFilter = songFilter.map( (el) => {
+        songFilter = songFilter.map( (el) => { // Usamos un .map para agregar ese tiempo al array filtado. 
             return {
                 id: el.id,
                 name: el.name,
@@ -100,13 +104,15 @@
         } );
         console.log("milestone 4");
         console.log(songFilter);    
-        printingMenu(songFilter);  
+        printingMenu(songFilter);  // Reimprime el "printedMenu" con los nuevos valores.
         console.log("milestone 5");  
-        finalPlaylist();
+        finalPlaylist(); // Activa la parte final del flujo.
     }
 
-    const finalPlaylist = () => {
+    // Llevamos a cabo varios querySelectors para borrar las distintas secciones que ya no se usarán, esto para mostrar que he podido usarlo, aunque probablemente haya una manera mas eficiente que agregarlos en la manera en que lo hice. 
+    const finalPlaylist = () => { 
         console.log("milestone 6");  
+        // Erasing everything.
         let eraseTimeInputs = document.querySelector(".actionArea p");
         eraseTimeInputs.remove();
         eraseTimeInputs = document.querySelector(".actionArea input");
@@ -114,6 +120,8 @@
         eraseTimeInputs = document.querySelector(".actionArea button");
         eraseTimeInputs.remove();
         console.log("milestone 7");  
+
+        // Imprimiendo la lista final con el filtrado y adiciones.
         printingFinalPlaylist = document.querySelector(".actionArea");
         console.log("milestone 8");  
         printingFinalPlaylist.innerHTML = "<p id='userPlaylist'></p>"
